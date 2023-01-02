aa3e8f43e74a18ef47e7fd64ee53b6cc5b9dcc88
Giovanni <gbismondo@gmail.com>
2023-01-02 15:21:43 +0100

    working

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   
-  <div class="container flex flex-row items-stretch divide-x divide-black mx-auto m-10">
-    <div class="md:block md:basis-1/4 pt-1 pl-4 pr-2 pb-4 bg-gray-1 rounded-l-xl hidden opacity-90 drop-shadow-xl">
+  <div class="container flex flex-row items-stretch md:divide-x divide-black mx-auto m-10">
+    <div class="md:block md:basis-1/4 pt-1 pl-4 pr-2 pb-4 bg-gray-1 rounded-l-xl hidden backdrop-blur-sm bg-gray-1/60 drop-shadow-xl">
 
       <div class="flex items-center h-9">
         <WindowButtons/>
@@ -9,15 +9,15 @@
 
       <p class="text-1xl text-gray-4 fond-bold text-sm mb-2 mt-2">👋 About me</p>
       <p class="text-gray-5 mb-2 bg-gray-2 rounded-md p-1">
-        <font-awesome-icon icon="fa-solid fa-arrows-to-dot" class="text-orange-1"/>
+        <font-awesome-icon icon="fa-solid fa-arrows-to-dot" class="text-orange-1 mr-1"/>
         Summary
       </p>
       <p class="text-gray-5 mb-2 p-1 hover:bg-gray-2 group hover:rounded-md">
-        <font-awesome-icon icon="fa-solid fa-route" class="text-orange-1 group-hover:rotate-45"/>
+        <font-awesome-icon icon="fa-solid fa-route" class="text-orange-1 group-hover:rotate-45 mr-1"/>
         Experiences
       </p>
       <p class="text-gray-5 p-1 hover:bg-gray-2 group hover:rounded-md">
-        <font-awesome-icon icon="fa-solid fa-gamepad" class="text-orange-1 group-hover:rotate-45"/>
+        <font-awesome-icon icon="fa-solid fa-gamepad" class="text-orange-1 group-hover:rotate-45 mr-1"/>
         Passions
       </p>
 
