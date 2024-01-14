4334f596f0ae30e96cd9c6f2a69fa831aef5de8e
Giovanni <gbismondo@gmail.com>
2024-01-14 18:42:26 +0100

    Minor changes

--- src/components/WindowButtons.vue
+++ src/components/WindowButtons.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="flex items-center text-center border-red-900 bg-red-1 shadow-inner rounded-full w-3 h-3" id="closebtn">
+  <div class="place-items-center text-center border-red-900 bg-red-1 shadow-inner rounded-full w-3 h-3 group">
+    <div class="invisible group-hover:visible text-black font-light text-xs leading-[0.9] ">🔥</div>
   </div>
-  <div class="ml-2 border-yellow-900 bg-yellow-1 shadow-inner rounded-full w-3 h-3" id="minbtn">
+  <div class="place-items-center text-center ml-2 border-yellow-900 bg-yellow-1 shadow-inner rounded-full w-3 h-3 group">
+    <div class="invisible group-hover:visible text-black font-light text-xs leading-[0.9]">😃</div>
   </div>
-  <div class="ml-2 border-green-900 bg-green-1 shadow-inner rounded-full w-3 h-3" id="maxbtn">
+  <div class="place-items-center ml-2 border-green-900 bg-green-1 shadow-inner rounded-full w-3 h-3 group">
+    <div class="invisible group-hover:visible text-black font-light text-xs leading-[0.9] ">🚀</div>
   </div>
 </template>
+<script setup lang="ts">
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
@@ -6,16 +6,16 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
 //See: https://fontawesome.com/docs/web/use-with/vue/add-icons
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import {library} from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faArrowsToDot, faRoute, faGamepad, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope, faCertificate, faMagnifyingGlass, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
-import { faWrench, faServer, faCubes, faHourglassStart } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faGithub, faDocker, faLinkedinIn, faSafari } from '@fortawesome/free-brands-svg-icons'
+import {faArrowsToDot, faRoute, faGamepad, faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope, faCertificate, faMagnifyingGlass, faArrowUpRightFromSquare} from '@fortawesome/free-solid-svg-icons'
+import {faWrench, faServer, faCubes, faHourglassStart} from '@fortawesome/free-solid-svg-icons'
+import {faTwitter, faGithub, faDocker, faLinkedinIn, faSafari} from '@fortawesome/free-brands-svg-icons'
 import Home from "./components/Home.vue";
 import Case from "./components/Case.vue";
 
@@ -41,20 +41,27 @@ library.add(faLinkedinIn);
 library.add(faSafari);
 
 
-
 const routes = [
-    { name: "home", path: '/', component: Home },
-    { name: "summary", path: '/home/summary', component: Home },
-    { name: "experiences", path: '/home/experiences', component: Home },
-    { name: "passions", path: '/home/passions', component: Home },
-    { name: "tools", path: '/home/tools', component: Home },
-    { name: "case", path: '/case', component: Case},
+    {name: "home", path: '/', component: Home},
+    {name: "summary", path: '/home/summary', component: Home},
+    {name: "experiences", path: '/home/experiences', component: Home},
+    {name: "passions", path: '/home/passions', component: Home},
+    {name: "tools", path: '/home/tools', component: Home},
+    {name: "case", path: '/case', component: Case},
 ] satisfies RouteRecordRaw[];
 
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (to.name) {
+            return {
+                el: document.getElementById("section-" + String(to.name)),
+                behavior: 'smooth',
+            }
+        }
+    }
 })
 
 const app = createApp(App);
