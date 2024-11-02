f07fa357718725068be071c0feba705a1de58d9e
Giovanni <gbismondo@gmail.com>
2024-11-02 12:50:29 +0100

    books

--- src/components/Home/Books.vue
+++ src/components/Home/Books.vue
@@ -8,15 +8,24 @@
     Here are some Tech books that I've read and I suggest to you:
   </p>
 
-  <div class="grid grid-cols-2 lg:grid-cols-4  gap-2 lg:gap-5 my-4 justify-between ">
+  <div class="grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-5 my-4 justify-between ">
     <a v-for="book in techBooks" :key="book.title" :href="book.url" :title="book.title"
        class="hover:transform hover:skew-y-6">
       <img :src="book.img" :alt="book.title" loading="lazy" class="max-h-[250px] max-w-[190px]"/>
     </a>
   </div>
-  
-  
-  
+
+  <p class="pt-5">
+    Here are some NON-tech books that I've enjoyed:
+  </p>
+
+  <div class="grid grid-cols-2 lg:grid-cols-4  gap-2 lg:gap-5 my-4 justify-between ">
+    <a v-for="book in readBooks" :key="book.title" :href="book.url" :title="book.title"
+       class="hover:transform hover:skew-y-6">
+      <img :src="book.img" :alt="book.title" loading="lazy" class="max-h-[250px] max-w-[190px]"/>
+    </a>
+  </div>
+
 
 </template>
 <script setup lang="ts">
@@ -66,7 +75,7 @@ const techBooks: IBookInfo[] = [{
   },
   {
     title: "Patterns of Enterprise Application Architecture",
-    url :"https://martinfowler.com/books/eaa.html",
+    url: "https://martinfowler.com/books/eaa.html",
     img: "https://martinfowler.com/books/eaa.jpg",
     preferred: true
   },
@@ -74,6 +83,12 @@ const techBooks: IBookInfo[] = [{
     title: "Agile Principles, Patterns, And Practices In C#",
     url: "https://www.oreilly.com/library/view/agile-principles-patterns/0131857258/",
     img: "https://learning.oreilly.com/library/cover/0131857258/250w/",
+    preferred: true
+  },
+  {
+    title: "Programming TypeScript: Making Your JavaScript Applications Scale (English Edition)",
+    url: "https://www.amazon.it/gp/product/B07R86FL4K/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+    img: "https://m.media-amazon.com/images/I/914Lo09RDcL._SY466_.jpg",
     preferred: true
   },
   {
@@ -98,5 +113,52 @@ const techBooks: IBookInfo[] = [{
     preferred: true
   }];
 
+
+const readBooks: IBookInfo[] = [{
+  title: "Una sera tra amici a Jinbōchō (libreria Morisaki Vol. 2)",
+  url: "https://www.amazon.it/gp/product/B0C24B1FPY/ref=ppx_yo_dt_b_search_asin_image?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/91xjH+odyeL._SY466_.jpg",
+  preferred: true
+}, {
+  title: "I miei giorni alla libreria Morisaki",
+  url: "https://www.amazon.it/gp/product/B09X78TF9H/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/91CgATVJL-L._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Tutto è qui per te",
+  url: "https://www.amazon.it/gp/product/B0CLY94M54/ref=ppx_yo_dt_b_search_asin_image?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/81SvI55sjKL._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Moon Lake",
+  url: "https://www.amazon.it/gp/product/B09RPFWHW6/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/81pYk15HU3L._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Elefante a sorpresa: Un'indagine di Hap & Leonard (Ciclo Hap & Leonard Vol. 14)",
+  url: "https://www.amazon.it/gp/product/B081SDQ5ZX/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/81HYSWAU5hL._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Una banda di idioti",
+  url: "https://www.amazon.it/Una-banda-idioti-John-Toole/dp/8892940228/ref=sr_1_fkmr0_1?__mk_it_IT=ÅMÅŽÕÑ&crid=1XX2K15XU57OS&dib=eyJ2IjoiMSJ9.qHl5O2EVk9rgRaRP2MRNSkeXayfn8J_RILsLdOs2Jh_JIm7sFgrsFQYchLoY-BtJfEMkV_iEE0Fu-QIHAzo7zr8YZQ8qZLnSHQCtEu54mg3cDG4YQsVZbbzbJaYgWDR5.xFv-cpJdRayM47-tD9fObni1rtSQI24gh_ad2Coj168&dib_tag=se&keywords=una+banda+di+idi&qid=1730547932&sprefix=una+banda+di+idi%2Caps%2C131&sr=8-1-fkmr0",
+  img: "https://m.media-amazon.com/images/I/61Yt4kW+cPS._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Su la testa (Urania)",
+  url: "https://www.amazon.it/gp/product/B0825GZ48C/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/81oBM9dUBAL._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Chiusi dentro (Urania)",
+  url: "https://www.amazon.it/gp/product/B01HE4QZMG/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/81LRZ5FCVKL._SY466_.jpg",
+  preferred: true
+}, {
+  title: "Doctor Sleep",
+  url: "https://www.amazon.it/gp/product/B00UKK425O/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+  img: "https://m.media-amazon.com/images/I/817oC9agCwL._SY466_.jpg",
+  preferred: true
+}];
 
 </script>
